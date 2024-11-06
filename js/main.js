98d9cd47bd21303326ed2d8b18da3559490d0652
Diego <145619634+diegoxavir@users.noreply.github.com>
2024-11-05 22:00:16 -0500

    Added more Javascript  for dynamic information
    Added javascript to send the hotspot descriptions dynamically, and added some css for the text boxes.

--- js/main.js
+++ js/main.js
@@ -1,34 +1,40 @@
 (() => {
   console.log("IIFE Fired");
 
-
-  //variables
+  // Variables
   const hotspots = document.querySelectorAll('.Hotspot');
   console.log(hotspots);
 
+  const hotspotDesc = [
+    "Touch Controls For Easy Listening",
+    "State Of The Art Noise Cancellation and Transparency",
+    "Sweat And Water Resistant",
+    "Fast Charging For Up To 5 Hours Of Listening"
+  ];
 
-//functions
-function showInfo(e) {
-  //console.log("show info called");
-  let selected  = document.querySelector(`button[slot="${e.currentTarget.slot}"] > div`);
+  // Functions
+  function showInfo(e) {
+    let selected = document.querySelector(`button[slot="${e.currentTarget.slot}"] > div`);
+    gsap.to(selected, 1, {autoAlpha: 1});
+  }
 
-  gsap.to(selected, 1, {autoAlpha: 1});
-}
+  function hideInfo(e) {
+    let selected = document.querySelector(`button[slot="${e.currentTarget.slot}"] > div`);
+    gsap.to(selected, 1, {autoAlpha: 0});
+  }
 
-function hideInfo(e) {
-  //console.log("hide into called");
-  let selected = document.querySelector(`button[slot="${e.currentTarget.slot}"] > div`)
-  gsap.to(selected, 1, {autoAlpha: 0});
-}
+//looping hotspots
+  hotspots.forEach((hotspot, index) => {
+    const paragraph = hotspot.querySelector('p');
 
+    if (paragraph) {
+      paragraph.textContent = hotspotDesc[index];
+    }
+  });
 
-
-//event listeners
-hotspots.forEach(hotspot => {
-  hotspot.addEventListener("mouseover", showInfo);
-  hotspot.addEventListener("mouseleave", hideInfo);
-})
-
-
-
+  // Event listeners
+  hotspots.forEach(hotspot => {
+    hotspot.addEventListener("mouseover", showInfo);
+    hotspot.addEventListener("mouseleave", hideInfo);
+  });
 })();
